785c7750face8aa0a00875b5e50aaea65d574b3d
BrettKendrick <123604258+BrettKendrick@users.noreply.github.com>
2023-07-25 19:47:16 -0700

    Settup win conditions and functions for the game

---
+++ game_logic.js
@@ -0,0 +1,41 @@
+const box = document.querySelectorAll(".box")
+const playerText = document.querySelector("#playerText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6],
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+function gameStart(){
+
+}
+
+function spotSelected(){
+
+}
+
+function spotUpdated(cell, index){
+
+}
+
+function changePlayer() {
+
+}
+
+function changePlayer() {
+
+}
+
+function checkWinner() {
+    
+}
