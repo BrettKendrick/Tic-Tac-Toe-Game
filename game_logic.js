980e4c5eeb4a40aa363316bb43d0879ae0128959
BrettKendrick <123604258+BrettKendrick@users.noreply.github.com>
2023-07-25 22:41:12 -0700

    JavaScript version 1.0. works but needs formatting

--- game_logic.js
+++ game_logic.js
@@ -1,41 +1,79 @@
-const box = document.querySelectorAll(".box")
-const playerText = document.querySelector("#playerText");
-const restartBtn = document.querySelector("#restartBtn");
-const winConditions = [
-[0, 1, 2],
-[3, 4, 5],
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6],
-];
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = Array.from(document.getElementsByClassName('box'))
 
-let options = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
-let running = false;
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
 
-function gameStart(){
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
 
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 
-function spotSelected(){
+function boxClicked(e) {
+    const id = e.target.id
 
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} wins!` 
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
+        }
+
+        if(playerHasWon() !==true){
+            playerText.innerHTML = `${currentPlayer} draws!`
+            let winning_blocks = playerHasWon()
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
 }
 
-function spotUpdated(cell, index){
 
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c]
+        }
+    }
+    return false
 }
 
-function changePlayer() {
+restartBtn.addEventListener('click', restart)
 
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach( box => {
+        box.innerText = ''
+        box.style.backgroundColor= ''
+    })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = X_TEXT
 }
 
-function changePlayer() {
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
-}
 
-function checkWinner() {
-    
-}
+startGame()
